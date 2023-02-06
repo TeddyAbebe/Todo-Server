f19dd240a069e1cf7b6ca8e4aa9d3ecc77a5ae58
Tewodros Abebe <teddyab729@gmail.com>
2023-02-06 16:43:44 +0300

    preparing for netlify deply

---
+++ server-local.js
@@ -0,0 +1,3 @@
+const app = require("./src/server");
+
+app.listen(4000, () => console.log("Running on Port 4000"));
---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const serverless = require("serverless-http");
+
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.send("Todo Api");
+});
+
+app.use("/.netlify/functions/todo", require("./routes/todo.route"));
+
+module.exports = app;
+module.exports.handler = serverless(app);
